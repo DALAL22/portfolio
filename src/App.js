afdef4d457e7f9680ef033ec1e19548546d38d41
DALAL22 <dalal.al.muteq@gmail.com>
2018-08-07 05:51:57 +0300

    bulid

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="l">
+        <header id="h22">
+          <h1 id="h33">DALAL ABDULLAH SAEED</h1>
+          </header>
+          <h3 id="h44">Edocation:</h3>
+          <p>king khalid university, computer scince(2019)</p>
+          <a href="https://en.wikipedia.org/wiki/King_Khalid_University">KKU LINK</a>
+          <h3 id="h55">Objective :</h3>
+          <ul>
+          <li> I develop my professional  skills in my field of work,  whether in the facility
+          I work in or attending courses outside my work, and filling  my spare time by improving  my professional level to the highest degree .</li>
+          <li> Become a successful and future successful coach </li>
+          </ul>
+          <h3 id="h66">Skills:</h3>
+          <ol>
+          <li>  communication.</li>
+          <li> Drawing and Design</li>
+          <li>LDiction</li>
+          </ol>
+          <h3 id="h77"> Languege</h3>
+          <p>English, Arabic</p>
+
+      </ div>
+    );
+  }
+}
+
+export default App;
